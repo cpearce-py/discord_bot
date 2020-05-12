a259aae0a4da53190410d783d71e62617c9915d6
cpearce-py <corin@pearce-web.co.uk>
2020-05-12 18:19:00 +0100

    Adding Twitch API, and Twitch Chat Client.

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-
 const  token = process.env.BOT_TOKEN;
+const prefix = '$';
 
 bot.on('ready', () => {
     console.log('This bot is online.');
 
 })
+
 
 bot.login(token);
