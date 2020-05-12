96075a8c03425301d5d3a7a31b23e2980718d56f
cpearce-py <corin@pearce-web.co.uk>
2020-05-12 19:34:54 +0100

    Setup Discord and Twitch oauth.

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 require('dotenv').config();
 
 
+const Twitch = require('twitch').default;
+const twitch_clientID = process.env.TWITCH_CLIENT_ID;
+const twitch_token = process.env.TWITCH_ACCESS_TOKEN;
+const twitchClient = Twitch.withCredentials(twitch_clientID, twitch_token);
+
+
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-const  token = process.env.BOT_TOKEN;
+const client = new Discord.Client();
+const token = process.env.BOT_TOKEN;
 const prefix = '$';
 
-bot.on('ready', () => {
-    console.log('This bot is online.');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
 
 })
 
 
-bot.login(token);
+client.login(token);
