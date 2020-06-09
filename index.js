c407cd48fcba9770c0d74d83889b56e91eaf45ba
cpearce-py <corin@pearce-web.co.uk>
2020-06-09 02:36:06 -0400

    Setup for ngrok, and webhook

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ require('dotenv').config();
 const ngrok = require('ngrok');
 const WebHookListener = require('twitch-webhooks').default;
 
-// (async function() {
-//     const url = await ngrok.connect(8000);
-
-// })();
-
-
 const Twitch = require('twitch').default;
 const twitch_clientID = process.env.TWITCH_CLIENT_ID;
 const twitch_token = process.env.TWITCH_ACCESS_TOKEN;
@@ -19,49 +13,49 @@ const bot = new Discord.Client();
 const token = process.env.BOT_TOKEN;
 const prefix = '$';
 
+// bot.login(token);
 
-bot.once('ready', () => {
-    console.log(`Logged in as ${bot.user.tag}!`);
+// bot.once('ready', () => {
+//     console.log(`Logged in as ${bot.user.tag}!`);
 
-});
+// });
 
-bot.login(token);
+
 
 
 
 async function streamWebhook(client){
+    try {
+        const listener = await WebHookListener.create(client, {
+        hostName: '2a3c4af71731.ngrok.io',
+        port: 8000,
+        reverseProxy: {  port:  443, ssl: true }
+        });
 
-    // const listener = await WebHookListener.create(client, {
-    // hostName: '86d78d2a.ngrok.io',
-    // port: 8000,
-    // reverseProxy: {  port:  443, ssl: true }
-    // });
+        listener.listen();
 
-    const myUser = await client.helix.users.getUserByName('Citobor');
+        const myUser = await client.helix.users.getUserByName('citotester');
+        console.log(`user is: ${myUser.id}`)
 
-    const stream = {
-        'hub.callback': '86d78d2a.ngrok.io',
-        'hub.mode':  'subscribe',
-        'hub.topic': `https://api.twitch.tv/helix/streams?user_id=${myUser.id}`,
-        'hub.lease_seconds': 864000
+        const stream = await client.helix.streams.getStreamByUserId(myUser);
+
+        const subscription = await listener.subscribeToStreamChanges(myUser, async (stream) => {
+            if (stream) {
+                console.log(`${streamID.userDisplayName} just went live with title: ${stream.title}`);
+            } else {
+                const user = await client.helix.users.getUserById(myUser);
+                console.log(`${user.displayName} just went offline`);
+            } }, 100000); 
+
+        
+    } catch (err) {
+
+        console.log(err)
     }
-
-    console.log(stream["hub.topic"])
-
-
-    // const subscription = await listener.subscribeToStreamChanges(myUser, async (stream?: HelixStream) => {
-    //     if (stream) {
-    //         console.log(`${stream.userDisplayName} just went live with title: ${stream.title}`);
-    //     } else {
-    //         const user = await client.helix.users.getUserById(myUser.id);
-    //         console.log(`${user.displayName} just went offline`);
-    //     } }); 
-
-    // listener.listen();
-
 
 
 };
+
 
 
 streamWebhook(client);
